b86decf6f149b95effaf08ebbd7b1501139242c6
DOMINGO VENEGAS <domingo.venegas@uc.cl>
2024-03-21 12:58:42 -0300

    mobile menu v1

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -110,7 +110,6 @@ export function HeaderMenu({menu, primaryDomainUrl, viewport}) {
       {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
         if (!item.url) return null;
 
-        // if the url is internal, we strip the domain
         const url =
           item.url.includes('myshopify.com') ||
           item.url.includes(publicStoreDomain) ||
--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,5 +1,5 @@
-import {Await} from '@remix-run/react';
-import {Suspense, useState} from 'react';
+import {Await, NavLink} from '@remix-run/react';
+import {Suspense, useRef, useState} from 'react';
 import {Aside} from '~/components/Aside';
 import {Footer} from '~/components/Footer';
 import {Header, HeaderMenu} from '~/components/Header';
@@ -90,15 +90,115 @@ function SearchAside() {
  * }}
  */
 function MobileMenuAside({menu, shop}) {
+  const storeRef = useRef();
+  const tapeRef = useRef();
+
+  function revealMenu(e, ref) {
+    if (e) {
+      ref.current.classList.remove('h-12');
+      ref.current.classList.add('h-fit');
+    } else {
+      ref.current.classList.remove('h-fit');
+      ref.current.classList.add('h-12');
+    }
+  }
+
   return (
     menu &&
     shop?.primaryDomain?.url && (
       <Aside id="mobile-menu-aside" heading="MENU">
-        <HeaderMenu
+        {/* <HeaderMenu
           menu={menu}
           viewport="mobile"
           primaryDomainUrl={shop.primaryDomain.url}
-        />
+        /> */}
+        <div className="flex flex-col h-3/4 ml-6">
+          <div
+            ref={storeRef}
+            className="h-12 overflow-hidden transition-all"
+            onMouseEnter={() => revealMenu(true, storeRef)}
+            onMouseLeave={() => revealMenu(false, storeRef)}
+          >
+            <div className="drop-down">TIENDA</div>
+            <div className="pl-7 mt-3 mb-3 border-l-2 border-[#e5d201]">
+              <div
+                ref={tapeRef}
+                className="h-12 overflow-hidden"
+                onMouseEnter={() => revealMenu(true, tapeRef)}
+                onMouseLeave={() => revealMenu(false, tapeRef)}
+              >
+                <div className="drop-down">TAPES</div>
+                <div className="pl-7 mt-3 mb-3 border-l-2 border-[#e5d201] max-h-36 overflow-auto">
+                  <NavLink
+                    prefetch="intent"
+                    to="/products/k-tape"
+                    className="nav-menu-item text-font"
+                  >
+                    K-TAPE
+                  </NavLink>
+                  <NavLink
+                    prefetch="intent"
+                    to="/products/zinc-oxide"
+                    className="nav-menu-item-option text-font mr-auto"
+                  >
+                    ZINC OXIDE WHITE
+                  </NavLink>
+                  <NavLink
+                    prefetch="intent"
+                    to="/products/zinc-oxide-tan"
+                    className="nav-menu-item-option text-font mr-auto"
+                  >
+                    ZINC OXIDE TAN
+                  </NavLink>
+                  <NavLink
+                    prefetch="intent"
+                    to="/products/eab-tear"
+                    className="nav-menu-item text-font mr-auto"
+                  >
+                    TEAR EAB
+                  </NavLink>
+                  <NavLink
+                    prefetch="intent"
+                    to="/products/non-tear-eab"
+                    className="nav-menu-item text-font mr-auto"
+                  >
+                    NON-TEAR EAB
+                  </NavLink>
+                  <NavLink
+                    prefetch="intent"
+                    to="/products/coban"
+                    className="nav-menu-item text-font"
+                  >
+                    COHESIVE BANDAGE
+                  </NavLink>
+                  <NavLink
+                    prefetch="intent"
+                    to="/products/fixation-tape"
+                    className="nav-menu-item text-font"
+                  >
+                    FIXATION TAPE
+                  </NavLink>
+                </div>
+              </div>
+              <div className="drop-down">PACKS</div>
+              <div className="drop-down">CATEGORIAS</div>
+            </div>
+          </div>
+          {/* <NavLink
+            prefetch="intent"
+            to="/shop"
+            className="drop-down"
+            onMouseEnter={() => handleMenuDisplay()}
+          >
+            TIENDA
+          </NavLink> */}
+          <NavLink prefetch="intent" to="/tutorials" className="drop-down">
+            TUTORIALES
+          </NavLink>
+          <NavLink prefetch="intent" className="drop-down">
+            CONTACTO
+          </NavLink>
+        </div>
       </Aside>
     )
   );
