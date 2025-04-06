f1a4f7b401dd2cccf2424506ec8487a9476ab3d8
DOMINGO VENEGAS <domingo.venegas@uc.cl>
2025-04-06 23:34:57 +0200

    update titulo packs banner

--- app/components/home/PacksBanner2.jsx
+++ app/components/home/PacksBanner2.jsx
@@ -18,7 +18,7 @@ export default function PickUpBanner2(props) {
         >
           <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col gap-8 justify-center px-24">
             <h1 className="title-font text-white text-5xl font-bold max-w-2xl">
-              Descubre nuestros Packs de Productos
+              NUESTROS PACKS
             </h1>
             <p className="text-font text-white text-lg mt-4 max-w-xl">
               Preparate para tu competencia con el mejor tape y precio. Explora
@@ -37,31 +37,6 @@ export default function PickUpBanner2(props) {
           </div>
         </div>
       </section>
-      {/* <section className="relative" ref={props.reference}>
-        <div className="h-full -z-10 absolute bg-[url('https://cdn.shopify.com/s/files/1/0643/3554/4485/files/packsZincOxide.jpg?v=1743942489')]"></div>
-        <div className="pickup-info">
-          <h1 className="title-font family-title py-4">
-            PREPARATE CON NUESTROS PACKS
-          </h1>
-          <div className="flex flex-col items-center justify-center flex-1 gap-11 mb-10">
-            <div className="flex gap-10 mobile-col">
-              <p className="text-font text-left my-auto pickup-info-text px-10">
-                <span className="text-[#e5d201] text-font">
-                  ¡No te lo pierdas!
-                </span>
-              </p>
-            </div>
-
-            <NavLink
-              to="/products/curso-taping-deportivo"
-              prefetch="intent"
-              className="browse-button-2 mx-auto"
-            >
-              Ver el curso
-            </NavLink>
-          </div>
-        </div>
-      </section> */}
     </>
   );
 }
